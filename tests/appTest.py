75a26e38c9ee972ba6ead81a543c3b0ce747777e
Guilherme Soares May Rios <136922444+guilhermerios21@users.noreply.github.com>
2026-03-23 22:56:06 +0000

    commit inicial

---
+++ tests/appTest.py
@@ -0,0 +1,30 @@
+import unittest
+from app.app import app
+
+
+
+
+    
+
+
+class AppTest(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_print_health_check(self):
+        response = self.app.get('/health-check')
+        self.assertEqual(200, response.status_code, "Erro no test_http_code!")
+        self.assertEqual("<h1>Hello, I'm Alive!</h1>", response.get_data(as_text=True)
+                          , "Erro no test_print_health_check!")
+    
+    def test_print_hello_success(self):
+        response = self.app.get('/hello?name=Guilherme')
+        self.assertEqual(200, response.status_code, "Erro no test_http_code!")
+        self.assertEqual("Hello, Guilherme!", response.get_data(as_text=True)
+                          , "Erro no test_print_hello_success!")
+
+    def test_print_hello_error_without_name(self):
+        response = self.app.get('/hello')
+        self.assertEqual(400, response.status_code, "Erro no status code de erro!")
+        self.assertEqual("Nome não informado", response.get_data(as_text=True), "Erro no test_print_hello_error_without_name!")
+        
